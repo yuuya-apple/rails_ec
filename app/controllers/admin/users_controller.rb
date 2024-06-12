62de42cb4df359a90fbbb184b7dec97695043301
kawa <yuuya.apple@me.com>
2024-06-13 08:20:55 +0900

    ユーザー登録

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,20 @@
+class Admin::UsersController < ApplicationController
+  def new
+    @user=User.new
+  end
+
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      redirect_to(items_url,notice:"ユーザー登録完了")
+    else
+      render :new,status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password,:password_confirmation)
+  end
+end
