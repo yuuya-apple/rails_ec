80a6dd1094db12e7f4e12ed6de12d1f8b8fe0329
kawa <yuuya.apple@me.com>
2024-06-13 08:19:56 +0900

    relation修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+
   has_one_attached :image
+
+  belongs_to:user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@
 
 Rails.application.routes.draw do
   root to: 'items#index'
-
   resources :items
+
+  namespace :admin do
+    resources :users
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
