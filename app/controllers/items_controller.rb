3152bd634e02ecd58f11d95bdd0f1b4df873db3c
kawa <yuuya.apple@me.com>
2024-06-14 04:43:31 +0900

    コンフリク解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ItemsController < ApplicationController
   def index
     @items = Item.all
@@ -7,7 +5,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @recent_items = Item.where.not(id: @item.id).order(created_at: 'DESC').limit(4)
+    @recent_items = Item.where.not(id:@item.id).order(created_at: 'DESC').limit(4)
   end
 
   def new; end
