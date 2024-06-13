fea83503f813e7446234a6df5511ac013d48fabb
kawa <yuuya.apple@me.com>
2024-06-13 14:27:37 +0900

    一時コミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   root to: 'items#index'
+
   resources :items
 
   namespace :admin do
     resources :users
     resources :items
   end
+
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+
+
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
