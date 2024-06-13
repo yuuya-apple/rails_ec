7b9744d72df1c1a9ee98636d27f3df8e7bf1da81
kawa <yuuya.apple@me.com>
2024-06-13 15:17:39 +0900

    ログインのコミット

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,27 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    user=User.find_by(name:session_params[:name])
+    if user&.authenticate(session_params[:password])
+      session[:user_id]=user.id
+      redirect_to(root_url,notice:"#{session[:user_id]}ログインしました")
+    else
+      flash.now[:alert]="ユーザー名またはパスワードが違います"
+      render :new ,status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to(root_url,notice:"ログアウトしました")
+  end
+
+
+private
+  def session_params
+    params.require(:session).permit(:name,:password)
+  end
+
+end
