f3f3a738336d905f4a42c0fe3f42ddabb70766e8
kawa <yuuya.apple@me.com>
2024-06-17 18:58:16 +0900

    カートsession管理の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  helper_method :cart_size
-  before_action :cart_size
-
   private
-
-  def cart_size
-    @cart_size = cart_initialize&.sum(&:quantity)
-  end
-
-  def cart_initialize
-    cart = session[:cart]
-    cart.nil? ? [] : cart.map { |c| CartDetail.new(c) }
-  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
