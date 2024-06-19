ee2de9bba1cbc90ecc8ed4407f8a0eb463a086d6
kawa <yuuya.apple@me.com>
2024-06-19 19:50:13 +0900

    heroku メーラー設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "myapp_production"
 
+  config.action_mailer.default_url_options = { host: 'immense-refuge-33257.herokuapp.com' }
+
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_caching = false
 
