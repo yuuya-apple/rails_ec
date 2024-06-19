b5c35ae2c0f5e3fb3fc2081b50aee3bc9512237c
kawa <yuuya.apple@me.com>
2024-06-19 19:57:01 +0900

    heroku メーラー設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,17 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'immense-refuge-33257.herokuapp.com' }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_caching = false
 
